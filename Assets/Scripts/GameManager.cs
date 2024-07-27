a3a204314c8eead2159f309b0a784eb5bee45878
matichanx3x3 <amqgamedev@gmail.com>
2024-07-27 10:59:30 +0200

    urp y algunos efectos

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,14 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public  bool enableMov = true;
     public  bool enableRot = true;
     public  bool inMiniGame = false;
-
+    private bool finishedMiniGame = false;
+    public int goodGame;
+    public int badGame;
+    public int diffGames;
+    public Volume ppVol;
+    public FilmGrain fmGrain;
     public enum GameState
     {
         normal,
@@ -30,12 +36,69 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        gm_State = GameState.normal;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if (finishedMiniGame)
+        {
+            CalculateMoral();
+            if (diffGames >0)
+            {
+                if (ppVol.profile.TryGet(out fmGrain))
+                {
+                    float intensity = fmGrain.intensity.value;
+                    fmGrain.intensity.Override(intensity + .1f);
+                }
+            }else if (diffGames < 0)
+            {
+                if (ppVol.profile.TryGet(out fmGrain))
+                {
+                    float intensity = fmGrain.intensity.value;
+                    fmGrain.intensity.Override(intensity - .1f);
+                }
+            }
+            finishedMiniGame = true;
+        }
+    }
+
     public void FinishingMinigame(GameState gm)
     {
         enableMov = true;
         enableRot = true;
         inMiniGame = false;
         gm_State = gm;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.lockState = CursorLockMode.Locked;
+        badGame++;
+    }
+
+    public void FinishingMinigame()
+    {
+        enableMov = true;
+        enableRot = true;
+        inMiniGame = false;
+        gm_State = GameState.glitch;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.lockState = CursorLockMode.Locked;
+        goodGame++;
+    }
+
+    public void CalculateMoral()
+    {
+        
+        diffGames = badGame - goodGame;
+        // negativo hace las cosas bien
+        // positivo hace las cosas mal
+        
     }
 
     public void EnterMiniGame()
@@ -43,6 +106,9 @@ public class GameManager : MonoBehaviour
         enableMov = false;
         enableRot = false;
         inMiniGame = true;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.lockState = CursorLockMode.Confined;
     }
 
     
